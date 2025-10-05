0b5905c64ad94971d9019a4531ce687b06e157a6
Creeper19472 <creeper19472@crpteam.club>
2025-10-05 21:05:57 +0800

    add try ... catch statement

--- src/serious_python/bin/package_command.dart
+++ src/serious_python/bin/package_command.dart
@@ -487,7 +487,7 @@ class PackageCommand extends Command {
     for (var entity in directory.listSync()) {
       if (globs.any((g) => g.matches(entity.path.replaceAll("\\", "/"))) &&
           await entity.exists()) {
-        verbose("Deleting ${entity.path}");
+        try {verbose("Deleting ${entity.path}");} catch (e) {}
         await entity.delete(recursive: true);
       } else if (entity is Directory) {
         if (await cleanupDirRecursive(entity, globs)) {
